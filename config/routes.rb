494135678c75fd46fdcf66199223a0f0bfc71e0e
Ricardo <ricardomoura1979@gmail.com>
2022-08-02 20:48:12 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users, except: [:index]
   root to: 'products#index' #like get '/' => 'products#index or get '/'=> { controller: 'products', action: 'index'}
   get '/about', to: 'about#show'
   resources :products, only: [:index, :show]
